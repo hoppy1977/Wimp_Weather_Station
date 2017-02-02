43cc4fec438044ed985dd0b338fe3274ee151f65
Shane Hopcroft <shanehopcroft@hotmail.com>
2017-02-02 22:30:42 +1100

    Updating the pin used to turn the voltage measurement relay on and off from D4 to D14.

--- Wimp_Weather_Station.ino
+++ Wimp_Weather_Station.ino
@@ -39,7 +39,7 @@ HTU21D myHumidity; //Create an instance of the humidity sensor
 // digital I/O pins
 const byte WSPEED = 3;
 const byte RAIN = 2;
-const byte VOLTAGE_RELAY = 4;
+const byte VOLTAGE_RELAY = 14;
 const byte STAT1 = 7;
 
 // analog I/O pins
